4fe9b7e0a93023b647060e5aaff35e1cc289c790
Frédéric Siffert <6359378+leNeo@users.noreply.github.com>
2026-06-08 12:01:38 +0200

    Handle spaces in native ODX launcher paths

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-RUNPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -e $RUNPATH/venv ]; then
-    source $RUNPATH/venv/bin/activate
-fi
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$RUNPATH/SuperBuild/install/lib
-export DYLD_LIBRARY_PATH=$RUNPATH/SuperBuild/install/lib
-python3 $RUNPATH/run.py "$@"
+set -e
 
+RUNPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -d "${RUNPATH}/venv" ]; then
+    source "${RUNPATH}/venv/bin/activate"
+fi
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+${LD_LIBRARY_PATH}:}${RUNPATH}/SuperBuild/install/lib"
+export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:+${DYLD_LIBRARY_PATH}:}${RUNPATH}/SuperBuild/install/lib"
+exec python3 "${RUNPATH}/run.py" "$@"
