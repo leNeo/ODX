631c783be4467056b3bd370e7593ff1851d79f8f
Frédéric Siffert <6359378+leNeo@users.noreply.github.com>
2026-06-08 07:01:47 +0200

    Build PoissonRecon with C++17 on macOS

--- SuperBuild/cmake/Patch-poissonrecon-apple-openmp.cmake
+++ SuperBuild/cmake/Patch-poissonrecon-apple-openmp.cmake
@@ -1,27 +1,40 @@
 file(READ "${SOURCE_FILE}" contents)
 
 set(modern_cflags
-    "CFLAGS += -Xclang -fopenmp -I${OPENMP_ROOT}/include -Wno-deprecated -std=c++14 -pthread -Wno-invalid-offsetof")
+    "CFLAGS += -Xclang -fopenmp -I${OPENMP_ROOT}/include -Wno-deprecated -std=c++17 -pthread -Wno-invalid-offsetof")
 set(modern_lflags
     "LFLAGS += -L${OPENMP_ROOT}/lib -lomp -lc++ -lpthread -L/opt/homebrew/lib")
 
-string(FIND "${contents}" "${modern_cflags}" modern_position)
-if(NOT modern_position EQUAL -1)
-    return()
-endif()
-
-set(legacy_cflags
+set(upstream_cflags
     "CFLAGS += -fopenmp -Wno-deprecated -std=c++14 -pthread -Wno-invalid-offsetof")
-set(legacy_lflags
+set(previous_cflags
+    "CFLAGS += -Xclang -fopenmp -I${OPENMP_ROOT}/include -Wno-deprecated -std=c++14 -pthread -Wno-invalid-offsetof")
+set(upstream_lflags
     "LFLAGS += -lgomp -lstdc++ -lpthread -L/opt/homebrew/lib")
 
-foreach(expected IN ITEMS "${legacy_cflags}" "${legacy_lflags}")
-    string(FIND "${contents}" "${expected}" expected_position)
-    if(expected_position EQUAL -1)
+string(FIND "${contents}" "${modern_cflags}" modern_cflags_position)
+if(modern_cflags_position EQUAL -1)
+    set(cflags_replaced FALSE)
+    foreach(candidate IN ITEMS "${upstream_cflags}" "${previous_cflags}")
+        string(FIND "${contents}" "${candidate}" candidate_position)
+        if(NOT candidate_position EQUAL -1)
+            string(REPLACE "${candidate}" "${modern_cflags}" contents "${contents}")
+            set(cflags_replaced TRUE)
+            break()
+        endif()
+    endforeach()
+    if(NOT cflags_replaced)
         message(FATAL_ERROR "Cannot patch PoissonRecon: expected compiler flags were not found")
     endif()
-endforeach()
+endif()
 
-string(REPLACE "${legacy_cflags}" "${modern_cflags}" contents "${contents}")
-string(REPLACE "${legacy_lflags}" "${modern_lflags}" contents "${contents}")
+string(FIND "${contents}" "${modern_lflags}" modern_lflags_position)
+if(modern_lflags_position EQUAL -1)
+    string(FIND "${contents}" "${upstream_lflags}" upstream_lflags_position)
+    if(upstream_lflags_position EQUAL -1)
+        message(FATAL_ERROR "Cannot patch PoissonRecon: expected linker flags were not found")
+    endif()
+    string(REPLACE "${upstream_lflags}" "${modern_lflags}" contents "${contents}")
+endif()
+
 file(WRITE "${SOURCE_FILE}" "${contents}")
