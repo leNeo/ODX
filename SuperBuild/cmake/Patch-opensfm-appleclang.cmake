ec5c5baa81834c8da95df1bc8e7f42adede83dd8
Frédéric Siffert <6359378+leNeo@users.noreply.github.com>
2026-06-08 07:25:09 +0200

    Support Ceres Jet abs in OpenSfM

--- SuperBuild/cmake/Patch-opensfm-appleclang.cmake
+++ SuperBuild/cmake/Patch-opensfm-appleclang.cmake
@@ -3,14 +3,32 @@ set(triangulation_header
 
 file(READ "${triangulation_header}" contents)
 
-set(old_abs "std::abs<T>(det)")
-set(new_abs "std::abs(det)")
+set(old_abs_block
+"#ifdef __aarch64__
+  if (std::abs<T>(det) < eps) {
+#else
+  if (abs(det) < eps) {
+#endif")
+set(intermediate_abs_block
+"#ifdef __aarch64__
+  if (std::abs(det) < eps) {
+#else
+  if (abs(det) < eps) {
+#endif")
+set(new_abs_block
+"  using std::abs;
+  if (abs(det) < eps) {")
 
-string(FIND "${contents}" "${old_abs}" old_abs_position)
-string(FIND "${contents}" "${new_abs}" new_abs_position)
+string(FIND "${contents}" "${old_abs_block}" old_abs_position)
+string(FIND "${contents}" "${intermediate_abs_block}" intermediate_abs_position)
+string(FIND "${contents}" "${new_abs_block}" new_abs_position)
 
 if(NOT old_abs_position EQUAL -1)
-    string(REPLACE "${old_abs}" "${new_abs}" contents "${contents}")
+    string(REPLACE "${old_abs_block}" "${new_abs_block}" contents "${contents}")
+    file(WRITE "${triangulation_header}" "${contents}")
+elseif(NOT intermediate_abs_position EQUAL -1)
+    string(REPLACE "${intermediate_abs_block}" "${new_abs_block}"
+        contents "${contents}")
     file(WRITE "${triangulation_header}" "${contents}")
 elseif(new_abs_position EQUAL -1)
     message(FATAL_ERROR
